b93b53545a3027079d7a00382b5f17ecfe8ffa56
Padman <frimpongy890@gmail.com>
2024-08-30 13:05:14 +0000

    few additions

--- lib/app/view/app.dart
+++ lib/app/view/app.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:heart_disease_mobile_app/bloc/cubit/heart_disease_cubit.dart';
 import 'package:heart_disease_mobile_app/counter/counter.dart';
 import 'package:heart_disease_mobile_app/l10n/l10n.dart';
+import 'package:heart_disease_mobile_app/pages/home_page.dart';
+import 'package:heart_disease_mobile_app/service/api_service.dart';
+// import 'package:flutter_localization/flutter_localization.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
@@ -8,6 +13,7 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         appBarTheme: AppBarTheme(
           backgroundColor: Theme.of(context).colorScheme.inversePrimary,
@@ -16,7 +22,7 @@ class App extends StatelessWidget {
       ),
       localizationsDelegates: AppLocalizations.localizationsDelegates,
       supportedLocales: AppLocalizations.supportedLocales,
-      home: const CounterPage(),
+      home: const HomePage(),
     );
   }
 }
