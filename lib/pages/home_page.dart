517a7b00a2c3d36c217074d2c9373a7a17051833
Padman <frimpongy890@gmail.com>
2024-09-03 17:26:40 +0000

    added validation to text form fields

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -2,6 +2,7 @@ import 'dart:ui';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:heart_disease_mobile_app/bloc/cubit/heart_disease_cubit.dart';
 import 'package:heart_disease_mobile_app/models/heart_disease_model.dart';
 import 'package:heart_disease_mobile_app/service/api_service.dart';
@@ -135,10 +136,19 @@ class _HomePageState extends State<HomePage> {
                     Builder(
                       builder: (context) {
                         return CustomTextField(
-                          // hintext: 'Enter Age',
-                          label: const Text('Enter Age'),
-                          icon: Icon(Icons.abc_sharp),
+                          hintext: 'Enter your age',
+                          label: const Text('Age'),
+                          icon: const Icon(FontAwesomeIcons.calendarDays),
                           controller: ageController,
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Please enter your age';
+                            }
+                            if (int.tryParse(value) == null) {
+                              return 'Please enter a valid integer';
+                            }
+                            return null;
+                          },
                         );
                       },
                     ),
@@ -159,20 +169,38 @@ class _HomePageState extends State<HomePage> {
                     Builder(
                       builder: (context) {
                         return CustomTextField(
-                          hintext: "Enter Resting blood Pressure",
-                          label: Text('Enter Resting blood Pressure'),
-                          icon: Icon(Icons.abc_sharp),
+                          hintext: "Enter your Resting blood Pressure",
+                          label: Text('Resting blood Pressure(Trestbps)'),
+                          icon: const Icon(FontAwesomeIcons.droplet),
                           controller: bloodPressureController,
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Please enter your resting blood pressure';
+                            }
+                            if (int.tryParse(value) == null) {
+                              return 'Please enter a valid integer';
+                            }
+                            return null;
+                          },
                         );
                       },
                     ),
                     Builder(
                       builder: (context) {
                         return CustomTextField(
-                          hintext: "Chol",
-                          label: Text('Serum Cholesterol'),
-                          icon: Icon(Icons.abc_sharp),
+                          hintext: 'Enter your cholesterol level',
+                          label: Text('Serum Cholesterol (Chol)'),
+                          icon: const Icon(FontAwesomeIcons.vial),
                           controller: cholesterolController,
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Please enter your cholesterol level';
+                            }
+                            if (int.tryParse(value) == null) {
+                              return 'Please enter a valid integer';
+                            }
+                            return null;
+                          },
                         );
                       },
                     ),
@@ -183,33 +211,49 @@ class _HomePageState extends State<HomePage> {
                       });
                     }),
 
-                    // Builder(
-                    //   builder: (context) {
-                    //     return CustomTextField(
-                    //       hintext: "FBS",
-                    //       label: Text('FBS'),
-                    //       icon: Icon(Icons.abc_sharp),
-                    //       controller: fbsController,
-                    //     );
-                    //   },
-                    // ),
                     Builder(
                       builder: (context) {
                         return CustomTextField(
-                          hintext: "restecg",
-                          label: Text('restecg'),
-                          icon: Icon(Icons.abc_sharp),
+                          hintext: 'Enter 0, 1 or 2 based on results',
+                          label: const Text(
+                            'Resting ECG Results (restecg)',
+                            style: TextStyle(
+                              fontSize: 15,
+                            ),
+                          ),
+                          icon: const Icon(FontAwesomeIcons.bedPulse),
                           controller: restecgController,
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Please enter a value for Restecg';
+                            }
+                            if (int.tryParse(value) == null) {
+                              return 'Please enter a valid integer';
+                            }
+                            return null;
+                          },
                         );
                       },
                     ),
                     Builder(
                       builder: (context) {
                         return CustomTextField(
-                          hintext: "thalach",
-                          label: Text('thalach'),
-                          icon: Icon(Icons.abc_sharp),
+                          hintext: 'Enter your maximum heart rate',
+                          label: const Text(
+                            'Max heart rate achieved(Thalach)',
+                            style: TextStyle(fontSize: 15),
+                          ),
+                          icon: const Icon(FontAwesomeIcons.heartPulse),
                           controller: thalachController,
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Please enter your maximum heart rate';
+                            }
+                            if (int.tryParse(value) == null) {
+                              return 'Please enter a valid integer';
+                            }
+                            return null;
+                          },
                         );
                       },
                     ),
@@ -232,10 +276,19 @@ class _HomePageState extends State<HomePage> {
                     Builder(
                       builder: (context) {
                         return CustomTextField(
-                          hintext: "oldPeak",
-                          label: Text('oldPeak'),
-                          icon: Icon(Icons.abc_sharp),
+                          hintext: "Enter the value for ST depression",
+                          label: Text('ST depression (oldPeak)'),
+                          icon: const Icon(FontAwesomeIcons.personWalking),
                           controller: oldpeakController,
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Please enter a value for Oldpeak';
+                            }
+                            if (int.tryParse(value) == null) {
+                              return 'Please enter a valid integer';
+                            }
+                            return null;
+                          },
                         );
                       },
                     ),
@@ -258,10 +311,26 @@ class _HomePageState extends State<HomePage> {
                     Builder(
                       builder: (context) {
                         return CustomTextField(
-                          hintext: "ca",
-                          label: Text('ca'),
-                          icon: Icon(Icons.abc_sharp),
+                          hintext: "Enter a value between 0 and 3",
+                          label: const Text(
+                            'Number of major Vessels ',
+                            // style: TextStyle(fontSize: 15),
+                          ),
+                          icon: const Icon(FontAwesomeIcons.heartCircleBolt),
                           controller: caController,
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return "Please enter a value";
+                            }
+                            if (int.tryParse(value) == null) {
+                              return 'Please enter a valid integer';
+                            }
+                            int? caValue = int.tryParse(value);
+                            if (caValue == null || caValue < 0 || caValue > 3) {
+                              return 'Value must be between 0 and 3';
+                            }
+                            return null;
+                          },
                         );
                       },
                     ),
@@ -296,26 +365,30 @@ class _HomePageState extends State<HomePage> {
                           height: 40,
                           child: ElevatedButton(
                               onPressed: () {
-                                final input = HeartDiseaseModel(
-                                  age: int.parse(ageController.text),
-                                  sex: _selectedGender,
-                                  cp: _selectedChestPain,
-                                  trestbps:
-                                      int.parse(bloodPressureController.text),
-                                  chol: int.parse(cholesterolController.text),
-                                  fbs: _selectedFbs,
-                                  restecg: int.parse(restecgController.text),
-                                  thalach: int.parse(thalachController.text),
-                                  exang: _selectedExang,
-                                  oldpeak: double.parse(oldpeakController.text),
-                                  slope: _selectedSlope,
-                                  ca: int.parse(caController.text),
-                                  thal: _selectedThal,
-                                );
-                                print(input);
-                                context
-                                    .read<HeartDiseaseCubit>()
-                                    .predictHeartDisease(input);
+                                if (_formKey.currentState!.validate()) {
+                                  final input = HeartDiseaseModel(
+                                    age: int.parse(ageController.text),
+                                    sex: _selectedGender,
+                                    cp: _selectedChestPain,
+                                    trestbps:
+                                        int.parse(bloodPressureController.text),
+                                    chol: int.parse(cholesterolController.text),
+                                    fbs: _selectedFbs,
+                                    restecg: int.parse(restecgController.text),
+                                    thalach: int.parse(thalachController.text),
+                                    exang: _selectedExang,
+                                    oldpeak:
+                                        double.parse(oldpeakController.text),
+                                    slope: _selectedSlope,
+                                    ca: int.parse(caController.text),
+                                    thal: _selectedThal,
+                                  );
+                                  print(input);
+                                  context
+                                      .read<HeartDiseaseCubit>()
+                                      .predictHeartDisease(input);
+                                }
+
                                 // final age = ageController.text;
                                 // final sex = sexController.text;
                                 // final BP = bloodPressureController.text;
